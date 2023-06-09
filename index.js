5b7d610692e99a0e92ff511bdce6175429585299
araza110 <aliraza@zenkoders.com>
2023-06-09 21:15:27 +0500

    `Firebase Init` for prompt.

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const app = express();
 const port = 3006;
 require('dotenv').config();
 
+const { getFirestore, collection, getDocs } = require("firebase/firestore");
+
+const { initializeApp } = require("firebase/app");
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const firebaseAppConfig = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseAppConfig);
 //use body parser
 app.use(express.json());
 
@@ -37,7 +54,11 @@ app.use("/thrive/api/chat", async (req, res) => {
 
   const { DOB, messages, uid, chatId } = req.body;
 
-  console.log("Date of Birth ==>", req.body);
+  const pr = collection(db, 'prompt')
+
+  const basePromptSnapshot = await getDocs(pr);
+
+  const basePrompt = basePromptSnapshot.docs.map(doc => doc.data());
 
   if (!uid || !chatId) {
     return res.status(400).json({ message: "uid or chatId is missing" });
@@ -45,17 +66,11 @@ app.use("/thrive/api/chat", async (req, res) => {
 
   const zodiacSign = getZodiacSign(DOB);
 
-  console.log('sign', zodiacSign);
-
-
-  //  let prompt =`Your client North Node sign is ${zodiacSign ?? ''}, and you are THRIVE AI, a personal life coach, motivational speaker, and an expert in personal growth, self-development, anxiety, depression, relationships, wellness, and overall well-being. You will answer questions to help users tap into their full potential, understand themselves and others better, and live the best life according to the North Node sign. Combine the power of AI with the expertise of a psychologist to provide personalized insights and strategies for growth and fulfillment based on the North Node sign. Using your North Node-based approach to personal growth and self-development, help users in identifying their strengths and weaknesses, and help them tap into their full potential and overcome challenges in a way that feels authentic and fulfilling.`;
-  // let prompt = `You are THRIVE AI, a personal life coach, motivational speaker, and an expert in personal growth, self-development, anxiety, depression, relationships, wellness, and overall well-being. In the discussions, you will always consider the influence of the North Node sign. You will always answer questions to help users tap into their full potential, understand themselves and others better, and live their best life according to the North Node sign. With the combined power of AI and the expertise of a psychologist, you will always provide personalized insights and strategies for growth and fulfillment based on the North Node sign. Using your North Node-based approach to personal growth and self-development, you will always help users in identifying their strengths and weaknesses, and help them tap into their full potential and overcome any challenges in a way that feels authentic and fulfilling. You will always start a discussions by introducing their north node signs.`;
-
   let prompt = `
   THRIVE AI, your personal growth companion, is here to assist you in navigating life's journey based on your North Node sign. Ask me anything related to personal growth, self-discovery, relationships, career, and well-being, and I'll provide tailored insights and strategies specific to your North Node sign. Together, let's unlock your full potential, overcome challenges, and live a fulfilling life aligned with your soul's purpose. Share your questions, and I'll guide you on your unique path with the wisdom of your North Node sign.
   `;
 
-  let message = [{ role: "system", content: prompt }, { role: "user", content: `my Zodiac sign is ${zodiacSign ?? ''}` }, ...messages];
+  let message = [{ role: "system", content: basePrompt[0].basePrompt }, { role: "user", content: `my Zodiac sign is ${zodiacSign ?? ''}` }, ...messages];
 
   let controller = null; // Store the AbortController instance
 
@@ -115,7 +130,7 @@ app.use("/thrive/api/chat", async (req, res) => {
         // Emit the content to connected sockets
         io.emit(`${uid}_${chatId}`, AIResponseObj);
 
-        console.log(`Received message from OpenAI: ${content}`);
+        // console.log(`Received message from OpenAI: ${content}`);
         // Update the UI with the new content
         messagesTemp += content;
       }
